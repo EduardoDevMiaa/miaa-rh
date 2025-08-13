3bb3543faa56877233a6f51a193a22b5e3ca78d2
AEscareno <armando.escareno@miaa.mx>
2025-08-13 08:46:32 -0600

    first commit

---
+++ src/main/java/com/miaa/rh/AministracionDeRecursosHumanosApplication.java
@@ -0,0 +1,13 @@
+package com.miaa.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AministracionDeRecursosHumanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AministracionDeRecursosHumanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miaa/rh/AministracionDeRecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaa.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AministracionDeRecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
